3f0fae7f81f0dfb2bfa6a76e17f178a7b9461db3
Max <maxbuydakov@gmail.com>
2021-04-10 20:18:11 -0700

    Added Ribosome code to repository
    Created main.py and Databank.json

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
+import os
+from random import choice, random
+import json
+
+data = json.load(open("Databank.json"))
+num = data["num"]
+nuc = data["nuc"]
+met = data["met"]
+ile = data["ile"]
+leu = data["leu"]
+phe = data["phe"]
+val = data["val"]
+thr = data["thr"]
+pro = data["pro"]
+ser = data["ser"]
+ala = data["ala"]
+lys = data["lys"]
+asn = data["asn"]
+gln = data["gln"]
+his = data["his"]
+stop = data["stop"]
+tyr = data["tyr"]
+glu = data["glu"]
+asp = data["asp"]
+arg = data["arg"]
+trp = data["trp"]
+cys = data["cys"]
+gly = data["gly"]
+
+
+def translate(aliases=["t"]):
+    print("Enter Your RNA Sequence")
+    RNA = input()
+    if "T" in RNA:
+        print ("Thymine cannot be present in the RNA sequence, make sure to recheck the sequence provided")
+    elif choice(num) in RNA:
+        print ("Integers should not be in the sequence. \n Make sure to remove the 5' and 3' if they are present")
+    else:
+        print(RNA)
+        if choice(met) in RNA:
+            print("met")
+        else:
+            pass
+    return translate()
+
+translate()
+    
+
 
